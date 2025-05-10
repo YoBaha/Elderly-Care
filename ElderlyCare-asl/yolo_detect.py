e8db1acc394d146c9696fff6130ae7ce9524419a
YoBaha <bahaeddine.benmansour@esprit.tn>
2025-05-10 19:55:09 +0100

    Add Conv2 to torch safe globals for YOLO modelsfd detc silu

--- ElderlyCare-asl/yolo_detect.py
+++ ElderlyCare-asl/yolo_detect.py
@@ -7,6 +7,9 @@ from ultralytics.nn.modules.conv import Conv
 from torch.nn.modules.container import Sequential
 from torch.nn.modules.conv import Conv2d  # Add this import*
 from torch.nn.modules.batchnorm import BatchNorm2d  # Add this
+from torch.nn.modules.activation import SiLU  # Add this
+from ultralytics.nn.modules.block import C2f, SPPF
+from ultralytics.nn.modules.head import Detect
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import torch.serialization
@@ -69,7 +72,7 @@ if not os.path.exists(model_path):
     exit(1)
 
 # Allowlist globals for torch.load
-torch.serialization.add_safe_globals([DetectionModel, Sequential, Conv, Conv2d,BatchNorm2d])
+torch.serialization.add_safe_globals([DetectionModel, Sequential, Conv, Conv2d,BatchNorm2d, C2f, SPPF, Detect, SiLU])
 model = YOLO(model_path, task='detect')
 labels = model.names
 print(f'Model labels: {labels}')
